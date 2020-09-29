c9c5226711dda1393c4444b753ece65fb8e87f2e
quarkusio <no-reply@quarkus.io>
2020-09-29 18:16:48 +0000

    Initial commit

---
+++ src/main/java/com/vibes/push/ExampleResource.java
@@ -0,0 +1,16 @@
+package com.vibes.push;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/hello")
+public class ExampleResource {
+
+    @GetMapping
+    public String hello() {
+        return "hello";
+    }
+}
